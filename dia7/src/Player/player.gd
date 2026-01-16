d41b8149421416bb5b14e0a5ec557346d87337cf
ramosdetrigo <julia.and.ramos@gmail.com>
2026-01-16 00:12:58 -0300

    Aula 7

---
+++ dia7/src/Player/player.gd
@@ -0,0 +1,131 @@
+class_name Player
+extends CharacterBody2D
+
+const MAX_HP := 50.0
+var current_hp := MAX_HP
+
+var dead := false
+
+var move_speed := 300
+var dash_speed := move_speed * 3
+
+var bullet_scene := preload("res://src/Player/bullet.tscn")
+var can_shoot := true
+var invunerable := false
+
+var dashing := false
+var can_dash := true
+var dash_direction := Vector2(1 ,0)
+
+
+func _ready() -> void:
+	Global.player = self
+
+
+func _process(_delta: float) -> void:
+	# Atualiza a posição da arma ao redor do player
+	var gun_direction := (get_global_mouse_position() - global_position).normalized()
+	$Gun.position = gun_direction * 40 # 40px na direção do mouse
+	# Faz a arma estar rotacionada na direção do mouse
+	$Gun.look_at(get_global_mouse_position())
+	
+	# Flipa o player e a arma se estiverem olhando pra esquerda
+	var mouse_pos := get_local_mouse_position()
+	if mouse_pos.x <= 0:
+		$AnimatedSprite2D.flip_h = true
+		# Flipa a arma na vertical pra ela não ficar de cabeça pra baixo
+		$Gun.flip_v = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+		$Gun.flip_v = false
+	
+	# Lógica de dash
+	if Input.is_action_just_pressed("dash") and can_dash:
+		# Inicia o dash
+		dashing = true
+		%DashingTimer.start()
+		
+		# Inicia o delay do dash
+		can_dash = false
+		%CanDashTimer.start()
+
+
+func _physics_process(delta: float) -> void:
+	if dead: return
+	
+	# Direção de movimento de acordo com os inputs
+	var dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	# Atualiza a direção do dash se o player não estiver dando dash
+	if not dashing and dir != Vector2(0,0):
+		dash_direction = dir
+	
+	# Move o player
+	velocity = dir * move_speed
+	if dashing:
+		velocity += dash_direction * dash_speed
+	
+	# Seta a animação de acordo com a velocidade atual
+	if velocity == Vector2(0,0):
+		$AnimatedSprite2D.play("idle")
+	else:
+		$AnimatedSprite2D.play("walk")
+	
+	# Lógica de tiro
+	if Input.is_action_pressed("shoot") and can_shoot:
+		# Cria uma nova bala
+		var bullet: RigidBody2D = bullet_scene.instantiate()
+		get_parent().add_child(bullet)
+		
+		# Atualiza a posição inicial da bala pra ser a mesma da arma
+		bullet.global_position = %Gun.global_position
+		# Atira a bala na direção em que a arma está apontada
+		var bullet_dir := Vector2.from_angle(%Gun.rotation)
+		bullet.apply_impulse(bullet_dir * 500)
+		
+		# Inicia o cooldown de tiro
+		can_shoot = false
+		%ShootTimer.start()
+	
+	# Movimenta o jogador e lida com possível colisãos
+	var collision := move_and_collide(velocity * delta)
+	if collision != null:
+		var body := collision.get_collider()
+		# Se o corpo colidido é um inimigo, toma dano
+		if body is Enemy:
+			_damage()
+
+
+func _damage() -> void:
+	if invunerable:
+		return
+	
+	# Animação de dano
+	$AnimatedSprite2D.modulate = Color("ff9080ff")
+	create_tween().tween_property($AnimatedSprite2D, "modulate", Color.WHITE, 0.5)
+	
+	# Deixa o player invulnerável por um tempo
+	invunerable = true
+	%InvTimer.start()
+	
+	# Diminui o HP e mata o player se HP < 0.0
+	current_hp -= 10.0
+	if current_hp <= 0.0:
+		hide()
+		dead = true
+		collision_layer = 0
+		collision_mask = 0
+
+
+#region Timeouts
+func _on_shoot_timer_timeout() -> void:
+	can_shoot = true
+
+func _on_inv_timer_timeout() -> void:
+	invunerable = false
+
+func _on_can_dash_timer_timeout() -> void:
+	can_dash = true
+
+func _on_dashing_timer_timeout() -> void:
+	dashing = false
+#endregion
---
+++ dia7/src/main.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var enemy_scene := preload("res://src/Enemy/enemy.tscn")
+var contador_morte: int = 0
+
+
+func rand_spawn_pos() -> Vector2:
+	# Escolhe um ponto aleatório entre 0 e 100% do progresso da linha de spawn
+	%SpawnSampler.progress_ratio = randf()
+	# + offset do local do player (faz o spawn seguir a posição do player)
+	# - offset do centro da tela (arrasta o ponto para fora da tela)
+	return %SpawnSampler.position + %Player.position - Vector2(576.0, 324.0)
+
+
+# Spawna inimigos conforme o timer SpawnTimer
+func _on_spawn_timer_timeout() -> void:
+	# Cria um novo inimigo em uma posição de spawn aleatória
+	var enemy: Enemy = enemy_scene.instantiate()
+	enemy.position = rand_spawn_pos()
+	# Adiciona ele como filho do nó de arena, pra meio que "conter"
+	# os elementos do jogo em um único lugar (ajuda pro menu de pausa, etc.)
+	%Arena.add_child(enemy)
+	enemy.died.connect(_enemy_died)
+
+
+func _enemy_died() -> void:
+	contador_morte += 1
+
+func _on_pause_button_toggled(pause: bool) -> void:
+	# Desativa o processamento do nó de arena
+	# (isso também desativa o processamento dos nós filhos! Ver documentação de "node > process_mode")
+	if pause:
+		%Arena.process_mode = Node.PROCESS_MODE_DISABLED
+	else:
+		%Arena.process_mode = Node.PROCESS_MODE_INHERIT
+	# Altera a visibilidade da tela de "jogo pausado"
+	%PauseLabel.visible = pause
+	
